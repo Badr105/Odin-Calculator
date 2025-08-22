433e7bc1e15eb9803d19135dec9639d93320caa3
BadrG <bguellass@gmail.com>
2025-08-22 13:25:23 +0100

    Added the main logic of calculator, but there is a bug where nothing gets displayed on the display

--- log.js
+++ log.js
@@ -1,36 +1,20 @@
-let n1, operator, n2;
+const display = document.getElementById("display");
 
-const display = document.querySelector(".display");
-
-function add(num1, num2) {
-    return num1 + num2;
+function appendToDisplay(input) {
+    display.value += input;
 }
 
-function subtract(num1, num2) {
-    return num1 - num2;
+function clearDisplay() {
+    display.value = "";
 }
 
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function operate(n1, operator, n2) {
-    switch (operator) {
-        case "+":
-            return add(n1, n2);
-        case "-":
-            return subtract(n1, n2);
-        case "x":
-            return multiply(n1, n2);
-        case "/":
-            return divide(n1, n2);
+function operate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error) {
+        display.value = "Error";
     }
 }
 
-
-
-
+console.log("i am here")
